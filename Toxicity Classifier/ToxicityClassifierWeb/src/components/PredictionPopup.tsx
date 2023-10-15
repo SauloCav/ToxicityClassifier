da49fed8b228f8b1d054dedf39a0c2f5a1d5c612
saulo cav <saulocavalcante25@hotmail.com>
2023-10-14 21:02:01 -0300

    Finaliza a estilização básica do projeto

--- Toxicity Classifier/ToxicityClassifierWeb/src/components/PredictionPopup.tsx
+++ Toxicity Classifier/ToxicityClassifierWeb/src/components/PredictionPopup.tsx
@@ -16,13 +16,19 @@ const PredictionPopup: React.FC<PredictionPopupProps> = ({ show, onClose, predic
     <div className={popupClasses}>
       <div className="popup-content">
         <div className="popup-header">
-          <h2 className="popup-text">Detalhes da análise</h2>
+          <h2 className="popup-text-h2">Detalhes da análise</h2>
         </div>
         <div className="popup-predictions">
           {predictions.map((prediction, index) => (
-            <p className="popup-text" key={index}>{types[index]}: {prediction}</p>
+            <p className="popup-text" key={index}>
+              <span className="type-text">{types[index]}</span>: 
+              <span className={parseFloat(prediction) >= 0.5 ? "prediction-high" : "prediction-low"}>
+                {parseFloat(prediction) >= 0.5 ? " Sim" : " Não"}
+              </span>
+            </p>
           ))}
         </div>
+
         <button className="popup-close-button" onClick={onClose}>Close</button>
       </div>
     </div>
